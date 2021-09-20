5cd129b71d4eaef0f93bbde7c80c133574b8d0fc
Daniel Hernandez Talla <hernandezdany41@gmail.com>
2021-09-19 18:25:41 -0700

    Creating Client Controller View and Model

---
+++ GIT_MVC/Models/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GIT_MVC.Models
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Profile Picture")]
+        public string ProfilePictureURL { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Biography")]
+        public string Bio { get; set; }
+    }
+}
